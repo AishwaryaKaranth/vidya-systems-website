faf0a291277bb7d03dab0d250f310849998b3407
Aishwarya Karanth <aishwarya.k@Technovert.com>
2021-08-01 10:44:59 +0530

    initial setup

---
+++ vidya-systems/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../Nav/Nav";
+
+const Home=()=>{
+    return(
+        <div className="container">
+           <h1>working</h1>
+           <Navigation/>
+        </div>
+    )
+}
+
+export default Home;
