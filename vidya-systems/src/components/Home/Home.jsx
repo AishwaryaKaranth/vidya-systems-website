e49312326d8fbff5635249152c1d8874a67fe44d
Aishwarya Karanth <aishwarya.k@Technovert.com>
2021-08-02 21:59:40 +0530

    fix navbar

--- vidya-systems/src/components/Home/Home.jsx
+++ vidya-systems/src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navigation from "../Nav/Nav";
+import styles from "./Home.module.scss";
 
 const Home=()=>{
     return(
-        <div className="container">
+        <div className={styles["container"]}>
            <h1>working</h1>
            <Navigation/>
         </div>
--- vidya-systems/src/components/Nav/Nav.jsx
+++ vidya-systems/src/components/Nav/Nav.jsx
@@ -1,28 +1,32 @@
 import React from "react";
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Container } from 'react-bootstrap';
+import Dropdown from 'react-multilevel-dropdown';
 import styles from "./Nav.module.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navigation=()=>{
     return(
-        <Navbar expand="lg" className={styles["color-nav"]}>
-            <Navbar.Brand href="#home">Vidya Systems</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+        <Navbar collapseOnSelect expand="lg" className={styles["color-nav"]}>
+            <Container className={styles["container"]}>
+            <Navbar.Brand href="#home"><span className={styles["link"]}>Vidya Systems</span></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className={styles["me-auto"]}>
+                    <Nav.Link href="#about-us"><span className={styles["link"]}>About Us</span></Nav.Link>
+                    <NavDropdown title="Dropdown" id={styles["basic-nav-dropdown"]}>
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#about-us"><span className={styles["link"]}>FAQ</span></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
-        </Navbar>
+            </Container>
+        </Navbar>   
     )
+    
 }
 
 export default Navigation;
