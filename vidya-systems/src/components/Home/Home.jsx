e0860825425de806e48abcfd437775b654798fc3
Aishwarya Karanth <aishwarya.k@Technovert.com>
2021-08-02 22:56:18 +0530

    add hero image

--- vidya-systems/src/components/Home/Home.jsx
+++ vidya-systems/src/components/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navigation from "../Nav/Nav";
 import styles from "./Home.module.scss";
+import background from "../../assets/bg.jpg";
 
 const Home=()=>{
     return(
         <div className={styles["container"]}>
            <h1>working</h1>
            <Navigation/>
+           <div className={styles["background"]}>
+                <img src={background} className={styles["layer"]}></img>
+                <p className={styles["hero-heading"]}>Dummy text</p>
+            </div>
+           
         </div>
     )
 }
